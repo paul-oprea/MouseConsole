3e85df36662d6df1b85564db82a0e63cc217091c
Paul Oprea <paul.oprea@gmail.com>
2019-01-19 00:00:07 +0000

    First Iteration

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int delay = int.Parse(args[0]);
+                while (true) {
+                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                    MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+                    //Console.WriteLine("Clicked");
+                    System.Threading.Thread.Sleep(delay * 1000);
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine("Could not parse argument as numeric");
+                Console.ReadKey();
+            }
+        }
+    }
+}
